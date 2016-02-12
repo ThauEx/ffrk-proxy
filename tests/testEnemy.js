2eebfb51941c5e6c851076b26005865a7bf4ce65
ThauEx <thau.exodus@gmail.com>
2016-02-12 13:52:20 +0100

    Add config support for supporter.js

--- tests/testEnemy.js
+++ tests/testEnemy.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var enemyFilter = require(__dirname + "/../lib/filter/enemy.js");
 
-var json = fs.readFileSync(__dirname + "/../dump/get_battle_init_data2.json").toString();
+var json = fs.readFileSync(__dirname + "/get_battle_init_data-morphing_boss.json").toString();
 try {
 	json = JSON.parse(json);
 } catch (e) {
