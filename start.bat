db62a65113edc2d64ea0da99612d674dc98f1a8c
ThauEx <github.com@thau.pw>
2018-04-06 17:39:27 +0200

    Remove start.ps1 and move logic into start.bat

--- start.bat
+++ start.bat
@@ -1,3 +1,7 @@
 @echo off
-powershell.exe -command "& '.\start.ps1'"
+bin\node.exe .\node_modules\babel-cli\bin\babel.js --plugins inline-json,..\babel\config-plugin.js lib\filter\*.js --out-dir public\
+del public\*.js
+move public\lib\filter\* public
+rd /s /q public\lib
+bin\node.exe bin/app.js
 pause
--- start.ps1
+++
@@ -1,5 +0,0 @@
-.\bin\node.exe .\node_modules\babel-cli\bin\babel.js --plugins inline-json,..\babel\config-plugin.js lib\filter\*.js --out-dir public\
-rm .\public\*.js
-mv .\public\lib\filter\* .\public
-rm -r -fo .\public\lib
-node bin/app.js
